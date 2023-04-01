5b94126bbda53711e42f696d7653ec19f26693cc
pybuk <pybuuuk@gmail.com>
2023-04-01 20:41:49 +1000

    Create mob's moving

--- class_files/classes.py
+++ class_files/classes.py
@@ -1,4 +1,5 @@
 import pygame
+import math
 import random
 
 #основной класс
@@ -168,20 +169,38 @@ class Archer(Player):
 
 
 class Mob():
-    def __init__(self, hp, damage, texture, speed, x, y, hitbox):
+    image = pygame.image.load("img/enemy/mobs/cats.png")
+    
+    def __init__(self, hp, damage, texture, speed, x, y):
         self.hp = hp
         self.damage = damage
         self.texture = texture
         self.speed = speed
         self.x = x
         self.y = y
-        self.hitbox = hitbox
+        self.hitbox = Mob.image.get_rect(topleft=(self.x, self.y))
 
-    def collision(self, bullets, mobs_list, mob1, mob2, mob3, mob4):
-        pass
+
+    def collision(player, mobs_list, bullets: list):
+        if len(mobs_list) > 0 and len(bullets) > 0:
+            for bullet in bullets:
+                hitbox = Mob.image.get_rect(topleft=(bullet.x, bullet.y))
+                print(hitbox)
+                for mob in mobs_list:
+                    if hitbox.colliderect(mob.hitbox):
+                        print("collision")
+                        if len(bullets) != 0:
+                            bullets.pop(bullets.index(bullet))
+                        mobs_list.pop(mobs_list.index(mob))
+                        print(mobs_list)
+                    
+        
+        return mobs_list
 
     def spawn(map, screen, xy, mobs_room, mob1, mob2, mob3, mob4, mobs_list):
+        
         if xy in mobs_room:
+
             if mob1 in mobs_list:
                 screen.blit(mob1.texture, (mob1.x, mob1.y))
             if mob2 in mobs_list:
@@ -190,8 +209,20 @@ class Mob():
                 screen.blit(mob3.texture, (mob3.x, mob3.y))
             if mob4 in mobs_list:
                 screen.blit(mob4.texture, (mob4.x, mob4.y))
-            if len(mobs_room) == 0:
+
+            if len(mobs_list) == 0:
                 mobs_room.remove(xy)
+                print("REMOVE")
+
+    def move_towards_player(mobs_list, player):
+        for mob in mobs_list:
+            dx, dy = player.x - mob.x, player.y - mob.y
+            dist = math.hypot(dx, dy)
+            dx, dy = dx / dist, dy / dist  # Normalize.
+            # Move along this normalized vector towards the player at current speed.
+            mob.x += dx * mob.speed
+            mob.y += dy * mob.speed
+
 #класс босса
 class Boss:
     def __init__(self, texture, hp, damage, x, y):
@@ -223,11 +254,14 @@ class Boss(Enemy):
 
 #класс стрелы
 class Arrow:
+    image = pygame.image.load("img/shoot/egg.png")
+
     def __init__(self, x, y, facing):
         self.x = x
         self.y = y
         self.facing = facing
         self.vel = 3
+        self.hitbox = Arrow.image.get_rect(topleft = (self.x, self.y))
 
     def draw(self, screen, egg):
         screen.blit(egg, (self.x, self.y))
